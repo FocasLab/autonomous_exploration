f7fa84247c9661995feec4d642ee7d792e7f63b0
DavidSundarsingh <sdavidsmith04@gmail.com>
2023-02-07 11:42:12 +0530

    Code to get the x,y,z coordinates added

--- autoexpl_ros/src/get_dynamic_location.cpp
+++ autoexpl_ros/src/get_dynamic_location.cpp
@@ -1,45 +1,33 @@
 #include <ros/ros.h>
-#include <iostream>
-#include <fstream>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
-
+#include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl/conversions.h>
-#include <pcl_ros/transforms.h>
-#include <tf/transform_listener.h>
 
-#include <geometry_msgs/Pose.h>
-#include <algorithm>
-#include <numeric>
+void cloud_callback (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg (*input, cloud);
 
-using namespace std;
-
-typedef pcl::PointXYZ PointType;
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-
-
-tf::TransformListener *tf_listener; 
-
- void cloud_cb(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input){
-
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input,pcl_pc2);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
-
-    cout << temp_cloud << endl;
-    //do stuff with temp_cloud here
+  // Iterate through the point cloud and print the x, y, and z coordinates
+  for (const auto& point : cloud.points)
+  {
+    std::cout << "Point: x = " << point.x << ", y = " << point.y << ", z = " << point.z << std::endl;
+  }
 }
 
-int main(int argc, char **argv){
+int main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "get_dynamic_location");
+  ros::NodeHandle nh;
 
-    ros::init(argc, argv, "get_dynamic_location");
-    ros::NodeHandle nh;
+  // Create a ROS subscriber for the PointCloud2 topic
+  ros::Subscriber sub = nh.subscribe ("\velodyne_points", 1, cloud_callback);
 
-    ros::Subscriber kinect_sub = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 1, cloud_cb);
+  // Spin and process ROS callbacks
+  ros::spin ();
 
-    return 0;
+  return 0;
 }
